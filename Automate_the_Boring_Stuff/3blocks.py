c64e4809fed89962a20032fd7fee7391bc831cd6
raymondwishart <raymondwishart@yahoo.com>
2017-12-12 15:41:23 -0500

    More scripts from Automate the Boring Stuff

---
+++ Automate_the_Boring_Stuff/3blocks.py
@@ -0,0 +1,11 @@
+spam = 8
+
+if spam == 10:
+    print('eggs')
+    if spam > 5:
+         print('bacon')
+    else:
+         print('spam')
+    print('spam')
+print('spam')
+
---
+++ Automate_the_Boring_Stuff/helloFunc2.py
@@ -0,0 +1,14 @@
+def hello(name):
+    print('Hello ' + name)
+
+
+hello('Alice')
+hello('bob')
+
+
+def howOld(age):
+    print('How old? ' + age)
+
+
+howOld('23')
+howOld('45')
